c5f6314c45bfef2d618e7654501c21a004e9392e
VibhuKhetharpal <vibhukhetharpal34@gmail.com>
2026-07-21 11:04:39 +0530

    Confirmed Working

--- server/server.js
+++ server/server.js
@@ -3,8 +3,13 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import listingsRoutes from './routes/listings.js';
+console.log("===== MY SERVER.JS IS RUNNING =====");
 
 const app = express();
+app.use((req, res, next) => {
+  console.log("Incoming:", req.method, req.url);
+  next();
+});
 app.use(cors());
 app.use(express.json());
 
