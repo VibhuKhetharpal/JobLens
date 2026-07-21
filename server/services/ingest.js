628ebd70957d08d93c2cbeabbee489d66173774b
VibhuKhetharpal <vibhukhetharpal34@gmail.com>
2026-07-21 10:30:45 +0530

    add ingestion service for Arbeitnow API

---
+++ server/services/ingest.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const Listing = require('../models/Listing');
+
+async function ingestListings() {
+  const response = await axios.get('https://www.arbeitnow.com/api/job-board-api');
+  const jobs = response.data.data;
+
+  for (const job of jobs) {
+    await Listing.findOneAndUpdate(
+      { externalId: job.slug },
+      {
+        title: job.title,
+        company: job.company_name,
+        location: job.location,
+        tags: job.tags,
+        remote: job.remote,
+        postedDate: new Date(job.created_at * 1000),
+        source: 'arbeitnow',
+        externalId: job.slug
+      },
+      { upsert: true, new: true }
+    );
+  }
+  console.log(`Ingested ${jobs.length} listings`);
+}
+
+module.exports = ingestListings;
